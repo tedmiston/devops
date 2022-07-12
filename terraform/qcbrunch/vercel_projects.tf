e56c3e012977c9da39c0459c3b7d054602ecc41a
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-12 15:29:16 -0400

    Update framework
    When framework is set to "Other", the correct value here seems to be `null` instead.

--- terraform/qcbrunch/vercel_projects.tf
+++ terraform/qcbrunch/vercel_projects.tf
@@ -1,7 +1,7 @@
 resource "vercel_project" "qcbrunch_dev" {
   id        = "QmZZyGn4FxMZ7iMg4um2S9Dmeb2ZVnp5xt2ojknBR2d7zg"
   name      = "qcbrunch-dev"
-  framework = ""
+  framework = null
 
   public_source              = false
   serverless_function_region = "sfo1"
@@ -10,7 +10,7 @@ resource "vercel_project" "qcbrunch_dev" {
 resource "vercel_project" "qcbrunch_prod" {
   id        = "QmSmPszZX5XpyfTqnjSQPus2FjkkDM1wEat1R41fwuTK6H"
   name      = "qcbrunch"
-  framework = ""
+  framework = null
 
   public_source              = false
   serverless_function_region = "sfo1"
