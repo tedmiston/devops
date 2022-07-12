9c96dcd603829a8e2e38cab72f891370008df482
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-12 15:05:33 -0400

    Make IDs explicit for existing resources

--- terraform/qcbrunch/vercel_dns.tf
+++ terraform/qcbrunch/vercel_dns.tf
@@ -39,8 +39,8 @@ resource "vercel_dns_record" "vercel_system_caa" {
 # sendgrid domain authentication (sender authentication)
 # - https://docs.sendgrid.com/ui/account-and-settings/how-to-set-up-domain-authentication
 
-# rec_7569dbe13a03d1a292e5f903
 resource "vercel_dns_record" "sendgrid_domain_authentication_2" {
+  id     = "rec_7569dbe13a03d1a292e5f903"
   domain = "qcbrunch.com"
   type   = "CNAME"
   name   = "s1._domainkey"
@@ -48,8 +48,8 @@ resource "vercel_dns_record" "sendgrid_domain_authentication_2" {
   ttl    = 86400
 }
 
-# rec_9188463caaa53c00f272de76
 resource "vercel_dns_record" "sendgrid_domain_authentication_1" {
+  id     = "rec_9188463caaa53c00f272de76"
   domain = "qcbrunch.com"
   type   = "CNAME"
   name   = "em2428"
@@ -57,8 +57,8 @@ resource "vercel_dns_record" "sendgrid_domain_authentication_1" {
   ttl    = 86400
 }
 
-# rec_f218a77416d00fb76fe5c3d4
 resource "vercel_dns_record" "sendgrid_domain_authentication_3" {
+  id     = "rec_f218a77416d00fb76fe5c3d4"
   domain = "qcbrunch.com"
   type   = "CNAME"
   name   = "s2._domainkey"
