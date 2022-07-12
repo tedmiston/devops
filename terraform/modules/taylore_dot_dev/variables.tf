99bc0a34578503dafa85111d21ae6bb2c345f493
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-12 18:16:50 -0400

    Make DRYer

---
+++ terraform/modules/taylore_dot_dev/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+  type        = string
+  default     = "taylore-dot-dev-www"
+  description = "Project name"
+}
+
+variable "vercel_region" {
+  type        = string
+  default     = "sfo1"
+  description = "Vercel Serverless Functions region"
+}
--- terraform/modules/taylore_dot_dev/vercel_projects.tf
+++ terraform/modules/taylore_dot_dev/vercel_projects.tf
@@ -1,17 +1,17 @@
 resource "vercel_project" "project_dev" {
   id        = "Qmdkf6qmoSBw2FMnsVQRpiRA7WCvSQqwDA5WRyp5kToM11"
-  name      = "taylore-dot-dev-www-dev"
+  name      = "${var.project}-dev"
   framework = "vue"
 
   public_source              = false
-  serverless_function_region = "sfo1"
+  serverless_function_region = var.vercel_region
 }
 
 resource "vercel_project" "project_prod" {
   id        = "QmRogMZ9PpryF3SkECwnosi6i1xjpEKQgEaSgZp4GzNvnx"
-  name      = "taylore-dot-dev-www-prod"
+  name      = "${var.project}-prod"
   framework = "vue"
 
   public_source              = false
-  serverless_function_region = "sfo1"
+  serverless_function_region = var.vercel_region
 }
