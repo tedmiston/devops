cfc44f6e48ea5ec60a0cba8f19a9e56ece504a58
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-12 15:03:05 -0400

    Increase Sendgrid TTLs

--- terraform/qcbrunch/vercel_dns.tf
+++ terraform/qcbrunch/vercel_dns.tf
@@ -45,9 +45,7 @@ resource "vercel_dns_record" "sendgrid_domain_authentication_2" {
   type   = "CNAME"
   name   = "s1._domainkey"
   value  = "s1.domainkey.u9701527.wl028.sendgrid.net."
-  # ttl    = 0
-  # ttl    = null
-  ttl = 60
+  ttl    = 86400
 }
 
 # rec_9188463caaa53c00f272de76
@@ -56,9 +54,7 @@ resource "vercel_dns_record" "sendgrid_domain_authentication_1" {
   type   = "CNAME"
   name   = "em2428"
   value  = "u9701527.wl028.sendgrid.net."
-  # ttl    = 0
-  # ttl    = null
-  ttl = 60
+  ttl    = 86400
 }
 
 # rec_f218a77416d00fb76fe5c3d4
@@ -67,7 +63,5 @@ resource "vercel_dns_record" "sendgrid_domain_authentication_3" {
   type   = "CNAME"
   name   = "s2._domainkey"
   value  = "s2.domainkey.u9701527.wl028.sendgrid.net."
-  # ttl    = 0
-  # ttl    = null
-  ttl = 60
+  ttl    = 86400
 }
