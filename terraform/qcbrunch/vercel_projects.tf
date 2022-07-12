dd8771dff8fcb4623f28c97616c98af47bfe3636
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-12 15:19:40 -0400

    Add Vercel projects

---
+++ terraform/qcbrunch/vercel_projects.tf
@@ -0,0 +1,17 @@
+resource "vercel_project" "qcbrunch_dev" {
+  id        = "QmZZyGn4FxMZ7iMg4um2S9Dmeb2ZVnp5xt2ojknBR2d7zg"
+  name      = "qcbrunch-dev"
+  framework = ""
+
+  public_source              = false
+  serverless_function_region = "sfo1"
+}
+
+resource "vercel_project" "qcbrunch_prod" {
+  id        = "QmSmPszZX5XpyfTqnjSQPus2FjkkDM1wEat1R41fwuTK6H"
+  name      = "qcbrunch"
+  framework = ""
+
+  public_source              = false
+  serverless_function_region = "sfo1"
+}
