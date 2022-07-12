41d6d9cb4447724a74503ee185eb2caf0bef0a7b
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-12 13:54:00 -0400

    Improve resource naming

--- terraform/qcbrunch/vercel.tf
+++ terraform/qcbrunch/vercel.tf
@@ -5,7 +5,7 @@
 # intentional or a bug is currently unclear).  the validation constraints also
 # differ between the terraform provider and the vercel web app.
 
-resource "vercel_dns_record" "cname_1" {
+resource "vercel_dns_record" "vercel_system_cname" {
   id     = "rec_cedd6cebbf44a21005a9321e"
   domain = "qcbrunch.com"
   type   = "CNAME"
@@ -14,7 +14,7 @@ resource "vercel_dns_record" "cname_1" {
   ttl    = 60
 }
 
-resource "vercel_dns_record" "a" {
+resource "vercel_dns_record" "vercel_system_a" {
   id     = "rec_0c999ce54a4bb0fd59179516"
   domain = "qcbrunch.com"
   type   = "A"
@@ -23,7 +23,7 @@ resource "vercel_dns_record" "a" {
   ttl    = 60
 }
 
-resource "vercel_dns_record" "caa" {
+resource "vercel_dns_record" "vercel_system_caa" {
   id     = "rec_d0c3daafcc2f548ef7ecac17"
   domain = "qcbrunch.com"
   type   = "CAA"
@@ -36,19 +36,11 @@ resource "vercel_dns_record" "caa" {
 
 # -- custom dns records --
 
-# rec_f218a77416d00fb76fe5c3d4
-resource "vercel_dns_record" "cname_2" {
-  domain = "qcbrunch.com"
-  type   = "CNAME"
-  name   = "s2._domainkey"
-  value  = "s2.domainkey.u9701527.wl028.sendgrid.net."
-  # ttl    = 0
-  # ttl    = null
-  ttl = 60
-}
+# sendgrid domain authentication (sender authentication)
+# - https://docs.sendgrid.com/ui/account-and-settings/how-to-set-up-domain-authentication
 
 # rec_7569dbe13a03d1a292e5f903
-resource "vercel_dns_record" "cname_3" {
+resource "vercel_dns_record" "sendgrid_domain_authentication_2" {
   domain = "qcbrunch.com"
   type   = "CNAME"
   name   = "s1._domainkey"
@@ -59,11 +51,22 @@ resource "vercel_dns_record" "cname_3" {
 }
 
 # rec_9188463caaa53c00f272de76
-resource "vercel_dns_record" "cname_4" {
+resource "vercel_dns_record" "sendgrid_domain_authentication_1" {
   domain = "qcbrunch.com"
   type   = "CNAME"
   name   = "em2428"
   value  = "u9701527.wl028.sendgrid.net."
+  # ttl    = 0
+  # ttl    = null
+  ttl = 60
+}
+
+# rec_f218a77416d00fb76fe5c3d4
+resource "vercel_dns_record" "sendgrid_domain_authentication_3" {
+  domain = "qcbrunch.com"
+  type   = "CNAME"
+  name   = "s2._domainkey"
+  value  = "s2.domainkey.u9701527.wl028.sendgrid.net."
   # ttl    = 0
   # ttl    = null
   ttl = 60
