5d09a3a50c20ffbd0f5d7709f2ef5877b7121b04
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-12 20:51:10 -0400

    Add boa

--- terraform/roots/prod/github/repos.tf
+++ terraform/roots/prod/github/repos.tf
@@ -7,3 +7,11 @@ resource "github_repository" "devops" {
 
   visibility = "public"
 }
+
+resource "github_repository" "boa" {
+  name         = "boa"
+  description  = "Boa is a Python package for normalizing and converting strings to snakecase."
+  homepage_url = "https://pypi.org/project/boa-str/"
+
+  visibility = "public"
+}
