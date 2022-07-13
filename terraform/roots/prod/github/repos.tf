d24f05343882d4db21290c1b6a2ec046405f32cd
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-12 20:49:06 -0400

    Add repo url

--- terraform/roots/prod/github/repos.tf
+++ terraform/roots/prod/github/repos.tf
@@ -1,8 +1,9 @@
 # -- public repos --
 
 resource "github_repository" "devops" {
-  name        = "devops"
-  description = "hello, world :robot:"
+  name         = "devops"
+  description  = "hello, world :robot:"
+  homepage_url = "https://github.com/tedmiston/devops"
 
   visibility = "public"
 }
