56030555d7a4659ef5ce4766c2f0e88fb0d9c4a8
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-19 15:58:17 -0400

    Add GitHub branch and default branch

--- terraform/roots/prod/github/repos.tf
+++ terraform/roots/prod/github/repos.tf
@@ -1,4 +1,6 @@
-# -- public repos --
+# == public repos ==
+
+# -- devops --
 
 resource "github_repository" "devops" {
   name         = "devops"
@@ -8,11 +10,23 @@ resource "github_repository" "devops" {
   visibility = "public"
 }
 
+resource "github_branch" "devops" {
+  repository = github_repository.devops.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "devops" {
+  repository = github_repository.devops.name
+  branch     = github_branch.devops.branch
+}
+
 resource "github_branch_protection" "devops" {
   repository_id = github_repository.devops.node_id
 
   pattern = "main"
 }
+
+# -- boa --
 
 resource "github_repository" "boa" {
   name         = "boa"
@@ -20,6 +34,16 @@ resource "github_repository" "boa" {
   homepage_url = "https://pypi.org/project/boa-str/"
 
   visibility = "public"
+}
+
+resource "github_branch" "boa" {
+  repository = github_repository.boa.name
+  branch     = "main"
+}
+
+resource "github_branch_default" "boa" {
+  repository = github_repository.boa.name
+  branch     = github_branch.boa.branch
 }
 
 resource "github_branch_protection" "boa" {
