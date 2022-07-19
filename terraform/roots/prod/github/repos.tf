f63c36ae3874b16d6e0377b19ae89329298e5eb4
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-19 16:01:00 -0400

    Format

--- terraform/roots/prod/github/repos.tf
+++ terraform/roots/prod/github/repos.tf
@@ -6,8 +6,7 @@ resource "github_repository" "devops" {
   name         = "devops"
   description  = "Terraform and related things :robot:"
   homepage_url = "https://github.com/tedmiston/devops"
-
-  visibility = "public"
+  visibility   = "public"
 }
 
 resource "github_branch" "devops" {
@@ -22,8 +21,7 @@ resource "github_branch_default" "devops" {
 
 resource "github_branch_protection" "devops" {
   repository_id = github_repository.devops.node_id
-
-  pattern = github_branch.devops.branch
+  pattern       = github_branch.devops.branch
 }
 
 # -- boa --
@@ -32,8 +30,7 @@ resource "github_repository" "boa" {
   name         = "boa"
   description  = "Boa is a Python package for normalizing and converting strings to snakecase."
   homepage_url = "https://pypi.org/project/boa-str/"
-
-  visibility = "public"
+  visibility   = "public"
 }
 
 resource "github_branch" "boa" {
@@ -48,6 +45,5 @@ resource "github_branch_default" "boa" {
 
 resource "github_branch_protection" "boa" {
   repository_id = github_repository.boa.node_id
-
-  pattern = github_branch.boa.branch
+  pattern       = github_branch.boa.branch
 }
