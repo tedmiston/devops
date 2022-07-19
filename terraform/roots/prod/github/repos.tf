2f9ccabc5d3e6db776f792b5d8b9367a52a92aaf
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-19 15:52:52 -0400

    Add GitHub branch protections

--- terraform/roots/prod/github/repos.tf
+++ terraform/roots/prod/github/repos.tf
@@ -8,10 +8,22 @@ resource "github_repository" "devops" {
   visibility = "public"
 }
 
+resource "github_branch_protection" "devops" {
+  repository_id = github_repository.devops.node_id
+
+  pattern = "main"
+}
+
 resource "github_repository" "boa" {
   name         = "boa"
   description  = "Boa is a Python package for normalizing and converting strings to snakecase."
   homepage_url = "https://pypi.org/project/boa-str/"
 
   visibility = "public"
+}
+
+resource "github_branch_protection" "boa" {
+  repository_id = github_repository.boa.node_id
+
+  pattern = "main"
 }
