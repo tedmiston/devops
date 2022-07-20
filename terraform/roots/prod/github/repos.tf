7c06ae3fdfa8614523a31564face953a578de3e1
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-19 20:06:17 -0400

    Improve naming

--- terraform/roots/prod/github/repos.tf
+++ terraform/roots/prod/github/repos.tf
@@ -2,7 +2,7 @@
 
 # -- devops --
 
-resource "github_repository" "devops" {
+resource "github_repository" "devops_repo" {
   name         = "devops"
   description  = "Terraform and related things :robot:"
   homepage_url = "https://github.com/tedmiston/devops"
@@ -10,23 +10,23 @@ resource "github_repository" "devops" {
 }
 
 resource "github_branch" "devops" {
-  repository = github_repository.devops.name
+  repository = github_repository.devops_repo.name
   branch     = "main"
 }
 
 resource "github_branch_default" "devops" {
-  repository = github_repository.devops.name
+  repository = github_repository.devops_repo.name
   branch     = github_branch.devops.branch
 }
 
 resource "github_branch_protection" "devops" {
-  repository_id = github_repository.devops.node_id
+  repository_id = github_repository.devops_repo.node_id
   pattern       = github_branch.devops.branch
 }
 
 # -- boa --
 
-resource "github_repository" "boa" {
+resource "github_repository" "boa_repo" {
   name         = "boa"
   description  = "Boa is a Python package for normalizing and converting strings to snakecase."
   homepage_url = "https://pypi.org/project/boa-str/"
@@ -34,16 +34,16 @@ resource "github_repository" "boa" {
 }
 
 resource "github_branch" "boa" {
-  repository = github_repository.boa.name
+  repository = github_repository.boa_repo.name
   branch     = "main"
 }
 
 resource "github_branch_default" "boa" {
-  repository = github_repository.boa.name
+  repository = github_repository.boa_repo.name
   branch     = github_branch.boa.branch
 }
 
 resource "github_branch_protection" "boa" {
-  repository_id = github_repository.boa.node_id
+  repository_id = github_repository.boa_repo.node_id
   pattern       = github_branch.boa.branch
 }
