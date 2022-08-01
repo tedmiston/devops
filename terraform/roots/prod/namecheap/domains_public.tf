42bcbf3de2e26366775a523257dfefe2fdb85f63
Taylor D. Edmiston <tedmiston@gmail.com>
2022-08-01 13:05:14 -0400

    Prettify comment

--- terraform/roots/prod/namecheap/domains_public.tf
+++ terraform/roots/prod/namecheap/domains_public.tf
@@ -1,19 +1,19 @@
-# keeping these all private for now, but they look like this:
+# keeping these all private for now, but the resources look like this:
 #
 # resource "namecheap_domain_records" "example_com" {
 #   domain = "example.com"
-
+#
 #   mode = "OVERWRITE"
-
+#
 #   email_type = "FWD"
-
+#
 #   record {
 #     type = "CNAME"
 #     hostname = "www"
 #     address = "parkingpage.namecheap.com."
 #     ttl = 1800
 #   }
-
+#
 #   # 1800 seconds is namecheap's default, but TTL doesn't seem to work for URL
 #   # Redirect Records anyway :/... or at least it doesn't show up in the GUI
 #   # console
@@ -23,7 +23,7 @@
 #     address = "http://www.example.com/"
 #     ttl = 1800
 #   }
-
+#
 #   # do NOT try to create TXT records here to mirror the built-in "mail settings"
 #   # records... that does not seem to be supported :( and instead makes a
 #   # duplicate record under "host records"
